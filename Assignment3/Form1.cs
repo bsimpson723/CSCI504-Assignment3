e36d5c98f0e40e9e44c91a99cf6d8a6e5a0ed251
XueLi <30300080+XueqiongLi@users.noreply.github.com>
2018-10-04 22:11:53 -0500

    added failedByMajorReport

--- Assignment3/Form1.cs
+++ Assignment3/Form1.cs
@@ -84,13 +84,48 @@ namespace Assignment3
         {
             if (validateCourseEntry(threshCourse_textBox.Text))
             {
+                var builder = new StringBuilder();
+                string[] words = threshCourse_textBox.Text.Split(' ');
+                var reportTitle = string.Format("Grade Threshold Report for ({0} {1})", words[0], words[1]);
+                builder.Append(reportTitle);
+                builder.Append(Environment.NewLine);
+                builder.Append("-------------------------------------------------------------------------");
+                builder.Append(Environment.NewLine);
+                var grades = new List<StudentGrade>();
+                if (threshLessThan_radio.Checked)
+                {
+                    grades = Program.m_studentGrades.ToList()
+                        .FindAll(x => x.DepartmentCode == words[0] &&
+                                      x.CourseNumber == Convert.ToUInt64(words[1]) &&
+                                      x.Grade.CompareTo(threshGrade_combo.SelectedItem) >= 0)
+                        .OrderBy(x => x.ZId).ToList();
+                }
+                else
+                {
+                    grades = Program.m_studentGrades.ToList()
+                        .FindAll(x => x.DepartmentCode == words[0] &&
+                                      x.CourseNumber == Convert.ToUInt64(words[1]) &&
+                                      x.Grade.CompareTo(threshGrade_combo.SelectedItem) <= 0)
+                        .OrderBy(x => x.ZId).ToList();
+                }
 
+                if (!grades.Any())
+                {
+                    builder.Append("No results match your query.");
+                }
+                foreach (var grade in grades)
+                {
+                    builder.Append(grade);
+                    builder.Append(Environment.NewLine);
+                }
+
+                queryResult_textBox.Text = builder.ToString();
             }
         }
 
         private bool validateCourseEntry(string course)
         {
-            string[] words = course.Split('-');
+            string[] words = course.Split(' ');
             if (words.Length < 2)
             {
                 //print error if input is not formatted correctly
@@ -107,6 +142,61 @@ namespace Assignment3
             }
 
             return true;
+        }
+        
+        private void showFailedByMajorReport_button_Click(object sender, EventArgs e)
+        {
+            // check whether selected a major
+            if (major_combo.SelectedIndex == -1)
+            {
+                queryResult_textBox.Text = "Error: Please select a major to search by.";
+                return;
+            }
+
+            // check whether typed course is valid
+            if (!validateCourseEntry(failedByMajorCourse_textBox.Text))
+            {
+                return;
+            }
+
+            //Get List and Build String for query results box
+            var builder = new StringBuilder();
+            var failMajorReportTitle = String.Format("Fail Report of Majors ({0}) in {1}", major_combo.SelectedValue, failedByMajorCourse_textBox.Text);
+            builder.Append(failMajorReportTitle);
+            builder.Append(Environment.NewLine);
+            builder.Append("-------------------------------------------------------------------------");
+            builder.Append(Environment.NewLine);
+
+            var majorStudents = Program.m_students.ToList()
+                .FindAll(x => x.Major == major_combo.SelectedItem.ToString())
+                .Select(x => x.ZId);
+
+            String[] words = failedByMajorCourse_textBox.Text.Split(' ');
+            var failMajorGrades = Program.m_studentGrades.ToList()
+                .FindAll(x => x.DepartmentCode == words[0].ToUpper() && x.CourseNumber.ToString() == words[1] && x.Grade == "F")
+                .FindAll(x => majorStudents.Contains(x.ZId))
+                .OrderBy(x => x.ZId);
+
+            // check whether matching grades empty
+            if (!failMajorGrades.Any())
+            {
+                builder.Append("No student matched the query criteria.");
+                builder.Append(Environment.NewLine);
+            }
+            else
+            {
+                foreach (var failed in failMajorGrades)
+                {
+                    builder.Append(failed);
+                    builder.Append(Environment.NewLine);
+                }
+            }
+
+            builder.Append(Environment.NewLine);
+            builder.Append("### END RESULTS ###");
+
+            // print appended stringbuilder to textbox
+            queryResult_textBox.Text = builder.ToString();
         }
     }
 }
